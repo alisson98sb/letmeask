59670faab9b295bc2e2091b0bda6e0f1b583db34
alisson98sb <alisson98sb@gmail.com>
2021-06-27 15:43:18 -0300

    New Build

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -91,7 +91,7 @@ export function Room(){
 								<span>{user.name}</span>
 							</div>
 						) : (
-							<span>Para enviar uma pergunta, <button>faça seu loguin</button>.</span>
+							<span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
 						)}
 						<Button type="submit" disabled={!user}>Enviar pergunta</Button>
 					</div>
